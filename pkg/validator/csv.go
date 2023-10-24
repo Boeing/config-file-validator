f7448de64d309fe4ec78f3d2b18d35e7aa528412
Jayu1214 <jayutrivedi1214@gmail.com>
2023-10-24 14:58:24 +0530

    Made changes in csv.go

--- pkg/validator/csv.go
+++ pkg/validator/csv.go
@@ -2,31 +2,60 @@ package validator
 
 import (
 	"bytes"
-	"io"
-
 	"encoding/csv"
+	"errors"
+	"io"
 )
 
-// CsvValidator is used to validate a byte slice that is intended to represent a
-// CSV file.
-type CsvValidator struct{}
+// CsvValidator is used to validate a byte slice that is intended to represent a CSV file.
+type CsvValidator struct {
+	ExpectedColumns int // The number of columns expected in each row
+	RequiredHeader  []string // Required header values
+}
+
+// ValidationError represents a validation error.
+type ValidationError struct {
+	Message string
+}
+
+func (e *ValidationError) Error() string {
+	return e.Message
+}
 
 // Validate checks if the provided byte slice represents a valid .csv file.
-// https://pkg.go.dev/encoding/csv
-func (csvv CsvValidator) Validate(b []byte) (bool, error) {
+func (csvv CsvValidator) Validate(b []byte) error {
 	csvReader := csv.NewReader(bytes.NewReader(b))
 	csvReader.TrimLeadingSpace = true
 
-	for {
-		_, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		}
+	// Read the header row
+	header, err := csvReader.Read()
+	if err != nil {
+		return &ValidationError{Message: "Error reading the header row"}
+	}
 
-		if err != nil {
-			return false, err
+	// Check the number of columns in the header
+	if csvv.ExpectedColumns > 0 && len(header) != csvv.ExpectedColumns {
+		return &ValidationError{Message: "Unexpected number of columns in the header"}
+	}
+
+	// Check for required header values
+	for _, requiredHeader := range csvv.RequiredHeader {
+		if !contains(header, requiredHeader) {
+			return &ValidationError{Message: "Missing required header value: " + requiredHeader}
 		}
 	}
 
-	return true, nil
+	// Additional custom validation logic can be added here
+
+	return nil // CSV is valid
+}
+
+// Helper function to check if a string exists in a slice of strings
+func contains(slice []string, str string) bool {
+	for _, s := range slice {
+		if s == str {
+			return true
+		}
+	}
+	return false
 }
