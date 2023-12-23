3a86053997d784ea794aba8e0930ac2d4e3efea3
Vladimir Siman <online@clew.cz>
2023-12-23 10:05:35 +0100

    code cleanup

--- pkg/validator/nginx.go
+++ pkg/validator/nginx.go
@@ -1,22 +1,12 @@
 package validator
 
 import (
-	"fmt"
 	"github.com/tufanbarisyildirim/gonginx/parser"
 )
 
 type NginxValidator struct{}
 
 func (nv NginxValidator) Validate(b []byte) (result bool, errMsg error) {
-	result = true
-
-	defer func() {
-		if r := recover(); r != nil {
-			result = false
-			errMsg = fmt.Errorf("%v", r)
-		}
-	}()
-
 	p := parser.NewStringParser(string(b))
 	_, err := p.Parse()
 	if err != nil {
