7550f12ecbc71392e05a65eebba4daa462a92c13
1garo <alevardai427@gmail.com>
2024-02-29 11:49:35 -0300

    feat: uncomment and fix other tests
    Missing "exlude file types set" test

--- cmd/validator/commands/flags_test.go
+++ cmd/validator/commands/flags_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func execute(t *testing.T, c *cobra.Command, args ...string) (string, error) {
+func ExecuteTestHelper(t *testing.T, c *cobra.Command, args ...string) (string, error) {
 	t.Helper()
 
 	buf := new(bytes.Buffer)
@@ -23,8 +23,7 @@ func execute(t *testing.T, c *cobra.Command, args ...string) (string, error) {
 }
 
 func Test_flags(t *testing.T) {
-	// We manipuate the Args to set them up for the testcases
-	// After this test we restore the initial args
+	// We manipulate the Args to set them up for the testcases
 	cases := []struct {
 		Name         string
 		Args         []string
@@ -35,23 +34,19 @@ func Test_flags(t *testing.T) {
 		{"flags set, wrong reporter", []string{"--exclude-dirs", "subdir", "--reporter", "wrong"}, 1},
 		{"flags set, json reporter", []string{"--exclude-dirs", "subdir", "--reporter", "json"}, 0},
 		{"flags set, junit reported", []string{"--exclude-dirs", "subdir", "--reporter", "junit"}, 0},
-		{"bad path", []string{"/path/does/not/exit"}, 1},
-		{"exclude file types set", []string{"--exclude-file-types", "json"}, 0},
-		//{"multiple paths", []string{"../../test/fixtures/subdir/good.json", "../../test/fixtures/good.json"}, 0},
-		// TODO: version is a command now, not a flag
-		//{"version", []string{"--version"}, 0},
-		//{"output set", []string{"--output=../../test/output", "--reporter=json", "."}, 0},
-		//{"empty string output set", []string{"--output", "", "--reporter", "json", "."}, 0},
-		//{"wrong output set", []string{"--output", "/path/not/exist", "--reporter", "json", "."}, 1},
-		//{"incorrect group", []string{"-groupby=badgroup", "."}, 1},
-		//{"correct group", []string{"-groupby=directory", "."}, 0},
+		{"bad path", []string{"--search_path", "/path/does/not/exit"}, 1},
+		//{"exclude file types set", []string{"--exclude-file-types", "json"}, 0},
+		{"multiple paths", []string{"../../../test/fixtures/subdir/good.json", "../../../test/fixtures/good.json"}, 0},
+		{"output set", []string{"--output", "../../../test/output", "--reporter", "json"}, 0},
+		{"empty string output set", []string{"--output", "", "--reporter", "json", "."}, 0},
+		{"wrong output set", []string{"--output", "/path/not/exist", "--reporter", "json", "."}, 1},
+		{"incorrect group", []string{"--groupby", "badgroup"}, 1},
+		{"correct group", []string{"--groupby", "directory"}, 0},
 	}
 
 	var exitStatus int
 
 	for _, tc := range cases {
-		// this call is required because otherwise flags panics,
-		// if args are set between flag.Parse call
 		fmt.Printf("Testing args: %v = %v\n", tc.Name, tc.Args)
 		root := &cobra.Command{
 			Use: "root",
@@ -60,7 +55,7 @@ func Test_flags(t *testing.T) {
 			}}
 		CmdFlags(root)
 
-		_, err := execute(t, root, tc.Args...)
+		_, err := ExecuteTestHelper(t, root, tc.Args...)
 		if err != nil {
 			t.Error(err)
 		}
