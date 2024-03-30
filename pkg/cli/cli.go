51ea14db17b9b98265fb4228c5385a2c686fd978
Hakan Vardar <hakovardo123@gmail.com>
2024-03-30 21:32:11 +0300

    Removed interface{} to solve type problems. > > Co-authored-by: ccoVeille <3875889+ccoVeille@users.noreply.github.com>

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -125,44 +125,57 @@ func (c CLI) printReports(reports []reporter.Report) error {
 		return nil
 	}
 
-	reportGroup, err := c.groupReports(reports, GroupOutput[0])
+	switch len(GroupOutput) {
+	case 1:
+		return c.printGroupSingle(reports)
+	case 2:
+		return c.printGroupDouble(reports)
+	case 3:
+		return c.printGroupTriple(reports)
+	default:
+		return fmt.Errorf("Invalid number of group outputs: %d", len(GroupOutput))
+	}
+}
+
+func (c CLI) printGroupSingle(reports []reporter.Report) error {
+	if GroupOutput[0] == "" {
+		return c.Reporter.Print(reports)
+	}
+
+	reportGroup, err := GroupBySingle(reports, GroupOutput[0])
 	if err != nil {
 		return err
 	}
-	// Group the output if the user specified a group by option
-	// Length is equal to one when empty as it contains an empty string
-	if len(GroupOutput) == 1 && GroupOutput[0] != "" {
-		// Check reporter type to determine how to print
-		if _, ok := c.Reporter.(reporter.JsonReporter); ok {
-			return reporter.PrintSingleGroupJson(reportGroup.(map[string][]reporter.Report))
-		}
-		return reporter.PrintSingleGroupStdout(reportGroup.(map[string][]reporter.Report))
-	} else if len(GroupOutput) == 2 {
-		if _, ok := c.Reporter.(reporter.JsonReporter); ok {
-			return reporter.PrintDoubleGroupJson(reportGroup.(map[string]map[string][]reporter.Report))
-		}
-		return reporter.PrintDoubleGroupStdout(reportGroup.(map[string]map[string][]reporter.Report))
-	} else if len(GroupOutput) == 3 {
-		if _, ok := c.Reporter.(reporter.JsonReporter); ok {
-			return reporter.PrintTripleGroupJson(reportGroup.(map[string]map[string]map[string][]reporter.Report))
-		}
-		return reporter.PrintTripleGroupStdout(reportGroup.(map[string]map[string]map[string][]reporter.Report))
-	}
-	return c.Reporter.Print(reports)
 
+	// Check reporter type to determine how to print
+	if _, ok := c.Reporter.(reporter.JsonReporter); ok {
+		return reporter.PrintSingleGroupJson(reportGroup)
+	}
+
+	return reporter.PrintSingleGroupStdout(reportGroup)
 }
 
-// groupReports groups the given reports based on the specified grouping option.
-// It returns the grouped reports and any error encountered during the grouping process.
-func (c CLI) groupReports(reports []reporter.Report, group string) (interface{}, error) {
-	switch len(GroupOutput) {
-	case 1:
-		return GroupBySingle(reports, group)
-	case 2:
-		return GroupByDouble(reports, GroupOutput)
-	case 3:
-		return GroupByTriple(reports, GroupOutput)
-	default:
-		return nil, fmt.Errorf("Invalid number of group outputs: %d", len(GroupOutput))
+func (c CLI) printGroupDouble(reports []reporter.Report) error {
+	reportGroup, err := GroupByDouble(reports, GroupOutput)
+	if err != nil {
+		return err
 	}
+
+	if _, ok := c.Reporter.(reporter.JsonReporter); ok {
+		return reporter.PrintDoubleGroupJson(reportGroup)
+	}
+
+	return reporter.PrintDoubleGroupStdout(reportGroup)
+}
+
+func (c CLI) printGroupTriple(reports []reporter.Report) error {
+	reportGroup, err := GroupByTriple(reports, GroupOutput)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := c.Reporter.(reporter.JsonReporter); ok {
+		return reporter.PrintTripleGroupJson(reportGroup)
+	}
+	return reporter.PrintTripleGroupStdout(reportGroup)
 }
