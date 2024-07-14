02f80f74638765ebe1e805adfb70d78824ad5d51
Hakan Vardar <hakovardo123@gmail.com>
2024-07-14 23:43:57 +0300

    Removed deprecated ParseBytes function

--- pkg/validator/editorconfig.go
+++ pkg/validator/editorconfig.go
@@ -1,6 +1,8 @@
 package validator
 
 import (
+	"bytes"
+
 	"github.com/editorconfig/editorconfig-core-go/v2"
 )
 
@@ -9,7 +11,7 @@ type EditorConfigValidator struct{}
 // Validate implements the Validator interface by attempting to
 // parse a byte array of an editorconfig file using editorconfig-core-go package
 func (EditorConfigValidator) Validate(b []byte) (bool, error) {
-	if _, err := editorconfig.ParseBytes(b); err != nil {
+	if _, err := editorconfig.Parse(bytes.NewReader(b)); err != nil {
 		return false, err
 	}
 	return true, nil
