847b923e9cf93aa6733cb7e507f4988fab06bbe8
shiina <152995083+shiina4119@users.noreply.github.com>
2024-08-23 22:16:25 +0530

    cmd/validator/validator_test.go: Fix typo.

--- cmd/validator/validator_test.go
+++ cmd/validator/validator_test.go
@@ -22,7 +22,7 @@ func Test_flags(t *testing.T) {
 		{"depth set", []string{"-depth=1", "."}, 0},
 		{"flags set, wrong reporter", []string{"--exclude-dirs=subdir", "--reporter=wrong", "."}, 1},
 		{"flags set, json reporter", []string{"--exclude-dirs=subdir", "--reporter=json", "."}, 0},
-		{"flags set, junit reported", []string{"--exclude-dirs=subdir", "--reporter=junit", "."}, 0},
+		{"flags set, junit reporter", []string{"--exclude-dirs=subdir", "--reporter=junit", "."}, 0},
 		{"bad path", []string{"/path/does/not/exit"}, 1},
 		{"exclude file types set", []string{"--exclude-file-types=json", "."}, 0},
 		{"multiple paths", []string{"../../test/fixtures/subdir/good.json", "../../test/fixtures/good.json"}, 0},
