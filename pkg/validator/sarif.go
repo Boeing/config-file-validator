a68aa85ddf5815bbeb604030dc0d896bf783ebe5
Clayton Kehoe <clayton.j.kehoe@boeing.com>
2025-10-22 14:25:57 -0500

    un gitignoring the pkg/validator

---
+++ pkg/validator/sarif.go
@@ -0,0 +1,29 @@
+package validator
+
+import (
+	"bytes"
+	"fmt"
+	v210 "github.com/owenrumney/go-sarif/v3/pkg/report/v210/sarif"
+	v22 "github.com/owenrumney/go-sarif/v3/pkg/report/v22/sarif"
+)
+
+type SarifValidator struct{}
+
+func (SarifValidator) Validate(b []byte) (bool, error) {
+	// Validate syntax
+	if bytes.Contains(b, []byte(`"version": "2.1.0"`)) {
+    	_, err := v210.FromBytes(b)
+		if err != nil {
+			return false, err
+		}
+	} else if bytes.Contains(b, []byte(`"version": "2.2"`)) {
+    	_, err := v22.FromBytes(b)
+		if err != nil {
+			return false, err
+		}
+	} else {
+		return false, fmt.Errorf("Unable to determine sarif version")
+	}
+
+	return true, nil
+}
