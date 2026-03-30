82b8a7ff9b4678a3f8bd1819d427faa0e55343c8
Clayton Kehoe <clayton.j.kehoe@boeing.com>
2026-03-30 13:02:40 -0500

    fix: pass main directly to testscript to avoid os.Exit in test code
    The main function already calls os.Exit(mainInit()), so passing it
    directly as the testscript command avoids any os.Exit calls in test
    files, satisfying revive's deep-exit rule.

--- cmd/validator/testscript_test.go
+++ cmd/validator/testscript_test.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"os"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
-func validatorCmd() {
-	os.Exit(mainInit())
-}
-
 func TestMain(m *testing.M) {
 	testscript.Main(m, map[string]func(){
-		"validator": validatorCmd,
+		"validator": main,
 	})
 }
 
